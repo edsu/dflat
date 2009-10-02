bb626d088d7d7ef1ef206d2289d5fa952d3df88a
Ed Summers <ehs@pobox.com>
2009-10-02 21:07:27 +0800

    include ez_setup.py in the sdist for pypi

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author = "Ed Summers",
     author_email = "ehs@pobox.com",
     url = "http://github.com/edsu/dflat",
-    py_modules = ['dflat'],
+    py_modules = ['dflat', 'ez_setup'],
     test_suite = 'test',
     scripts = ['bin/dflat']
 )
